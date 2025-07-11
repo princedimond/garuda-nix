d375f80a150a0a890d6426f566cdb6e541393784
princedimond <princedimond@gmail.com>
2025-07-11 08:43:39 -0400

    Initial commit

---
+++ PD-8Y7BYZ1/flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "Garuda-NIX";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    garuda.url = "gitlab:garuda-linux/garuda-nix-subsystem/stable";
+    zen-browser.url = "github:MarceColl/zen-browser-flake";
+    nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=latest";
+    plasma-manager = {
+      url = "github:nix-community/plasma-manager";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        home-manager.follows = "home-manager";
+      };
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+     };
+  };
+
+  outputs = 
+    inputs@{ 
+      self, 
+      nixpkgs, 
+      garuda, 
+      home-manager, 
+      plasma-manager,
+      zen-browser, 
+      nix-flatpak, 
+      ... 
+    }: 
+ {
+    nixosConfigurations = {
+      PD-8Y7BYZ1 = garuda.lib.garudaSystem {
+        system = "x86_64-linux";
+        modules = [
+          nix-flatpak.nixosModules.nix-flatpak
+          ./configuration.nix
+        ];
+      };
+    };
+  };
+}
