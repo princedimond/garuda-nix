cbe78f1bd932e5dcb79b931f447ba4b00f2eab5d
princedimond <princedimond@gmail.com>
2025-08-16 17:29:22 -0400

    updated packages
    updated all packages and installed teamviewer

---
+++ PD-BRFMF72/flake.nix
@@ -0,0 +1,71 @@
+{
+  description = "Garuda-NIX";
+
+  nixConfig.extra-substituters = [
+    "https://nyx.chaotic.cx"
+  ];
+
+   nixConfig.extra-trusted-public-keys = [
+     "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
+     "nyx.chaotic.cx-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
+   ];
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    garuda.url = "gitlab:garuda-linux/garuda-nix-subsystem/stable";
+    nixvim.url = "github:dc-tec/nixvim";
+    zen-browser.url = "github:MarceColl/zen-browser-flake";
+    nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=latest";
+    plasma-manager = {
+      url = "github:nix-community/plasma-manager";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        home-manager.follows = "home-manager";
+      };
+    };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      garuda,
+      nixvim,
+      home-manager,
+      plasma-manager,
+      zen-browser,
+      nix-flatpak,
+      ...
+    }:
+    let
+      lib = nixpkgs.lib;
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      nixosConfigurations = {
+        PD-BRFMF72 = garuda.lib.garudaSystem {
+          inherit system;
+          specialArgs = {
+            inherit inputs;
+          };
+          modules = [
+            nix-flatpak.nixosModules.nix-flatpak
+            ./configuration.nix
+          ];
+        };
+      };
+      /*
+          homeConfigurations = {
+            PD-5CG9235MQ9 = home-manager.lib.homeManagerConfiguration {
+              modules = [ ./home.nix ];
+              inherit pkgs;
+            }
+          };
+      */
+    };
+}
