37ed6cdf2280ef3afedeb8139343345de426a004
princedimond <princedimond@gmail.com>
2025-08-24 18:44:00 -0400

    add libimobiledevice
    added libimobile device for working with iphones

--- PD-BRFMF72/configuration.nix
+++ PD-BRFMF72/configuration.nix
@@ -176,6 +176,9 @@
     caligula
     keybase
     keybase-gui
+    kbfs
+    libimobiledevice
+    libimobiledevice-glue
     inputs.zen-browser.packages.x86_64-linux.default
     inputs.zen-browser.packages.x86_64-linux.specific
     inputs.zen-browser.packages.x86_64-linux.generic
