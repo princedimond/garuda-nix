7dc282a82dcb87aeb1510ecd712d081d6b6e27f6
princedimond <princedimond@gmail.com>
2025-09-15 20:02:40 -0500

    timezone update
    updated timezone

--- PD-BRFMF72/configuration.nix
+++ PD-BRFMF72/configuration.nix
@@ -73,7 +73,7 @@
   programs.neovim.vimAlias = true;
 
   # Set your time zone.
-  time.timeZone = "America/New_York";
+  time.timeZone = "America/Chicago";
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
