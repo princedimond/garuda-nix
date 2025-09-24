05f539754717353d56b4d07a7ca60566b008f931
princedimond <princedimond@gmail.com>
2025-09-24 16:18:20 -0400

    added barrier
    installed barrier

--- PD-GG8QRF2/flake.nix
+++ PD-GG8QRF2/flake.nix
@@ -5,10 +5,10 @@
     "https://nyx.chaotic.cx"
   ];
 
-   nixConfig.extra-trusted-public-keys = [
-     "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
-     "nyx.chaotic.cx-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
-   ];
+  nixConfig.extra-trusted-public-keys = [
+    "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
+    "nyx.chaotic.cx-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
+  ];
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
